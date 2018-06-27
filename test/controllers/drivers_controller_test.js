4d963e251c5bdc6e7b067a8b5533bcd00338a1b1
Brad Beltowski <bradbeltowski@gmail.com>
2018-06-27 19:39:23 -0400

    Changed model name

--- test/controllers/drivers_controller_test.js
+++ test/controllers/drivers_controller_test.js
@@ -3,19 +3,19 @@ const mongoose = require('mongoose');
 const request = require('supertest');
 const app = require('../../app');
 
-const Drivers = mongoose.model('driver');
+const Driver = mongoose.model('driver');
 
-describe('Drivers controller', () => {
+describe('Driver controller', () => {
   // method > route > result
   it('POST to /api/drivers creates a new driver', done => {
-    // get count of drivers
-    Drivers.count().then(count => {
+    // get count of driverd
+    Driver.count().then(count => {
       //
       request(app)
         .post('/api/drivers')
-        .send({ email: 'test@test.com' })
+        .send({ email: 'test@driver.com' })
         .end(res => {
-          Drivers.count().then(newCount => {
+          Driver.count().then(newCount => {
             //
             asssert((count = 1 === newCount));
             done();
@@ -27,7 +27,7 @@ describe('Drivers controller', () => {
   it('PUT to /api/drivers edits an existing driver', done => {
     // create driver for test
     const email = 'test@driver.com';
-    const driver = new Drivers({ email, driving: false });
+    const driver = new Driver({ email, driving: false });
     // edit driver
     driver.save().then(() => {
       // hit database for driver
@@ -46,13 +46,13 @@ describe('Drivers controller', () => {
   });
 
   it.only('DELETE to /api/driver removes a driver', done => {
-    const driver = new Drivers({ email: 'test@driver.com' });
+    const driver = new Driver({ email: 'test@driver.com' });
 
     driver.save().then(() => {
       request(app)
         .delete(`/api/drivers/${driver._id}`)
         .end(() => {
-          Drivers.findOne({ email: 'test@driver.com' }).then(driver => {
+          Driver.findOne({ email: 'test@driver.com' }).then(driver => {
             console.log('here?', driver);
             assert(driver === null);
             done();
